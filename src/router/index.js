5f03a3dfea3081353e819952b00d9925307f9d79
xuanhuanAI <3345968050@qq.com>
2026-07-22 10:53:10 +0800

    fix: 移除路由base参数修复刷新跳转

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory("/fakillboss/"),
+  history: createWebHashHistory(),
   routes,
 });
 
@@ -63,4 +63,6 @@ router.beforeEach((to, from, next) => {
 });
 
 export default router;
+
+
 
